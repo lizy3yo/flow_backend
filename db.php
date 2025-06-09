26616b069cbab189d85cf30be4643c0e664086e2
unknown <dejesuskharl32@gmail.com>
2025-06-09 20:37:25 +0800

    das

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 define('DB_HOST', 'mysql-flow-app.alwaysdata.net');
 define('DB_USER', 'flow-app');
 define('DB_PASS', 'kier253022');
-define('DB_NAME', 'flow-app_queue	');
+define('DB_NAME', 'flow-app_queue');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
