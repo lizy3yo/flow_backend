af7c249dc3a4c253ad3f524313360e069959c4f1
unknown <dejesuskharl32@gmail.com>
2025-06-10 05:21:18 +0800

    d

--- send-otp.php
+++ send-otp.php
@@ -1,14 +1,20 @@
 <?php
 
 require_once __DIR__ . '/db.php';
-require '../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-$env = parse_ini_file('.env');
+$env = parse_ini_file(__DIR__ . '/.env');
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
