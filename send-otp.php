16478bc5fe26716ed910e0ab1164ad321179942a
unknown <dejesuskharl32@gmail.com>
2025-06-10 05:27:50 +0800

    d

--- send-otp.php
+++ send-otp.php
@@ -5,10 +5,11 @@ require __DIR__ . '/vendor/autoload.php';
 
 $env = parse_ini_file(__DIR__ . '/.env');
 
+header('Access-Control-Allow-Origin: https://flow-i3g6.vercel.app');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
